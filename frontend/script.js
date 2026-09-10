20c1b972de246b481aa830f955fca293edaebf54
venugopal999 <venugopal.gora@gmail.com>
2026-09-10 18:33:28 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,12 @@
 async function runCode() {
   const code = document.getElementById("code").value;
   const language = document.getElementById("language").value;
+  const input = document.getElementById("input").value;
 
   const response = await fetch("https://online-compiler-srho.onrender.com/run", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ language, code })
+    body: JSON.stringify({ language, code, input })
   });
 
   const result = await response.json();
